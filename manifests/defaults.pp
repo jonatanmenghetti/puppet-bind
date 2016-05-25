d113fe2523f0edf12da5708befa3bbc4371d9b23
Jonatan Meneghetti <jonatan@unimestre.com>
2016-05-25 17:58:35 -0300

    Create defaults class

---
+++ manifests/defaults.pp
@@ -0,0 +1,21 @@
+class bind::defaults (
+    $supported              = undef,
+    $confdir                = undef,
+    $namedconf              = undef,
+    $cachedir               = undef,
+    $random_device          = undef,
+    $bind_user              = undef,
+    $bind_group             = undef,
+    $bind_package           = undef,
+    $bind_service           = undef,
+    $nsupdate_package       = undef,
+    $managed_keys_directory = undef,
+    $default_zones_warning  = undef,
+) {
+    unless is_bool($supported) {
+        fail('Please ensure that the dependencies of the bind module are installed and working correctly')
+    }
+    unless $supported {
+        fail('Platform is not supported')
+    }
+}
