691e9d478908a1a7a1dd8f652916b68cf2f17689
Jonatan Meneghetti <djony@Jonatans-MacBook-Pro-2.local>
2016-06-03 00:03:00 -0300

    Adicionado suporte a views

---
+++ manifests/view.pp
@@ -0,0 +1,23 @@
+define bind::view (
+    $match_clients                = 'any',
+    $match_destinations           = '',
+    $servers                      = {},
+    $zones                        = [],
+    $recursion                    = true,
+    $recursion_match_clients      = 'any',
+    $recursion_match_destinations = '',
+    $recursion_match_only         = false,
+    $notify_source                = '',
+    $also_notify                  = '',
+    $empty_zones                  = '',
+    $order                        = '10',
+    $minimal_responses            = false,
+) {
+    $confdir = $::bind::confdir
+
+    concat::fragment { "bind-view-${name}":
+        order   => $order,
+        target  => "${::bind::confdir}/views.conf",
+        content => template('bind/view.erb'),
+    }
+}
